781f8a86c2794695d422d0dc6117dd3b5f36a683
Chris Geelhoed <cgeelhoed@wearebraid.com>
2017-09-11 18:13:01 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<style src="./global.scss" lang="scss"></style>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
